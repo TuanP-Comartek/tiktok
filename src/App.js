97b4f83007bc52fc99965916d8b00d84bddf77bd
TuanP-Comartek <tuanp@comartek.vn>
2022-09-26 11:10:34 +0700

    building layout common

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layout";
 
 function App() {
   return (
@@ -8,8 +9,24 @@ function App() {
         <Routes>
           {publicRoutes.map((route, index) => {
             const Page = route.component;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = <></>;
+            }
+
             return (
-              <Route key={index} path={route.path} element={<Page />} />
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
             );
           })}
         </Routes>
